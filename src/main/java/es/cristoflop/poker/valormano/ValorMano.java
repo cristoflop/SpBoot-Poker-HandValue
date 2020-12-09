10ddad500290fe2e84cc47db562c9044d2c90d78
cristoflop <cristofer.lc.99@gmail.com>
2020-12-09 12:19:48 +0100

    Initial commit

---
+++ src/main/java/es/cristoflop/poker/valormano/ValorMano.java
@@ -0,0 +1,13 @@
+package es.cristoflop.poker.valormano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorMano {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorMano.class, args);
+    }
+
+}
