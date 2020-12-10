2993433a967656745eb0f2b3318b5d57a43f7153
cristoflop <cristofer.lc.99@gmail.com>
2020-12-10 15:02:11 +0100

    Added impl for trio check

--- src/main/java/es/cristoflop/poker/valormano/domain/checkers/CheckerTrio.java
+++ src/main/java/es/cristoflop/poker/valormano/domain/checkers/CheckerTrio.java
@@ -3,6 +3,7 @@ package es.cristoflop.poker.valormano.domain.checkers;
 import es.cristoflop.poker.valormano.domain.Carta;
 import es.cristoflop.poker.valormano.domain.ValorJugada;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CheckerTrio extends Checker {
@@ -13,6 +14,19 @@ public class CheckerTrio extends Checker {
 
     @Override
     protected List<Carta> cartasJugada(List<Carta> cartas) {
-        return null;
+        List<Carta> trio = new ArrayList<>();
+        int i = cartas.size() - 1;
+        boolean encontrado = false;
+        while (!encontrado && i > 2) {
+            if (cartas.get(i).getValor() == cartas.get(i - 2).getValor()) {
+                encontrado = true;
+                trio.add(cartas.get(i));
+                trio.add(cartas.get(i - 1));
+                trio.add(cartas.get(i - 2));
+            }
+            i++;
+        }
+        return trio;
     }
+
 }
