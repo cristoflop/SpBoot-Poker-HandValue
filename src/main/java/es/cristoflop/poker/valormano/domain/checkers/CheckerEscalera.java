485d87526b6dc6107efb85dc8c45678f02765efc
cristoflop <cristofer.lc.99@gmail.com>
2020-12-19 00:51:37 +0100

    Added methods to check if is a color or royal stailr

--- src/main/java/es/cristoflop/poker/valormano/domain/checkers/CheckerEscalera.java
+++ src/main/java/es/cristoflop/poker/valormano/domain/checkers/CheckerEscalera.java
@@ -1,9 +1,11 @@
 package es.cristoflop.poker.valormano.domain.checkers;
 
 import es.cristoflop.poker.valormano.domain.Carta;
+import es.cristoflop.poker.valormano.domain.ValorCarta;
 import es.cristoflop.poker.valormano.domain.ValorJugada;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CheckerEscalera extends Checker {
@@ -20,35 +22,29 @@ public class CheckerEscalera extends Checker {
     protected List<Carta> cartasJugada(List<Carta> cartas) {
         List<List<Carta>> posiblesEscaleras = new ArrayList<>();
 
-        for (int i = 0; i < cartas.size(); i++) { // posibles escaleras de tamaño 1
+        for (Carta carta : cartas) { // posibles escaleras de tamaño 1
             List<Carta> aux = new ArrayList<>();
-            aux.add(cartas.get(i));
+            aux.add(carta);
             posiblesEscaleras.add(aux);
         }
 
         int tamEscaleras = 1;
-        this.muestra(tamEscaleras, posiblesEscaleras);
         while (tamEscaleras < 5 && !posiblesEscaleras.isEmpty()) {
             posiblesEscaleras = this.addUnaCartaMasEnPosiblesEscalerasSiSePuede(posiblesEscaleras, cartas);
             tamEscaleras++;
-            this.muestra(tamEscaleras, posiblesEscaleras);
         }
 
-        List<Carta> bestStair = null;
-        if (!posiblesEscaleras.isEmpty()) bestStair = posiblesEscaleras.get(0);
-
-        return bestStair;
+        return posiblesEscaleras.isEmpty() ? Collections.emptyList() : this.mejorEscalera(posiblesEscaleras);
     }
 
     private List<List<Carta>> addUnaCartaMasEnPosiblesEscalerasSiSePuede(List<List<Carta>> posiblesEscaleras, List<Carta> cartas) {
         List<List<Carta>> nuevasEscaleras = new ArrayList<>();
-        for (int i = 0; i < posiblesEscaleras.size(); i++) {
-            List<Carta> posibleEscaleraActual = posiblesEscaleras.get(i);
+        for (List<Carta> posibleEscaleraActual : posiblesEscaleras) {
             Carta ultimaCarta = posibleEscaleraActual.get(posibleEscaleraActual.size() - 1);
-            for (int j = 0; j < cartas.size(); j++) {
-                if (ultimaCarta.esSiguiente(cartas.get(j))) {
+            for (Carta carta : cartas) {
+                if (ultimaCarta.esSiguiente(carta)) {
                     List<Carta> nuevaPosibleEscalera = new ArrayList<>(posibleEscaleraActual);
-                    nuevaPosibleEscalera.add(cartas.get(j));
+                    nuevaPosibleEscalera.add(carta);
                     nuevasEscaleras.add(nuevaPosibleEscalera);
                 }
             }
@@ -67,6 +63,45 @@ public class CheckerEscalera extends Checker {
         System.out.println();
         System.out.println("---------------------------");
         System.out.println();
+    }
+
+    private List<Carta> mejorEscalera(List<List<Carta>> escaleras) {
+        assert !escaleras.isEmpty();
+        List<Carta> mejorEscalera = escaleras.get(0);
+        this.valorJugada = this.valorEscalera(mejorEscalera);
+        for (List<Carta> escalera : escaleras) {
+            ValorJugada valorJugada = this.valorEscalera(escalera);
+            if (valorJugada.ordinal() > this.valorJugada.ordinal()) {
+                mejorEscalera = escalera;
+                this.valorJugada = valorJugada;
+            }
+        }
+        return mejorEscalera;
+    }
+
+    private ValorJugada valorEscalera(List<Carta> cartas) {
+        assert !cartas.isEmpty();
+        if (this.esEscaleraReal(cartas))
+            return this.royal;
+        else if (this.esEscaleraDeColor(cartas))
+            return this.color;
+        else
+            return this.regular;
+    }
+
+    private boolean esEscaleraDeColor(List<Carta> cartas) {
+        assert !cartas.isEmpty();
+        for (int i = 1; i < cartas.size(); i++) {
+            if (cartas.get(i - 1).suited(cartas.get(i)))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean esEscaleraReal(List<Carta> cartas) {
+        return this.esEscaleraDeColor(cartas) &&
+                cartas.get(0).getValor() == ValorCarta.AS &&
+                cartas.get(1).getValor() == ValorCarta.REY;
     }
 
 }
