fd6ab80c14e48d8c2d9628dee7a8469d94f2ed35
cristoflop <cristofer.lc.99@gmail.com>
2020-12-18 18:07:41 +0100

    Refactor method to check stair

--- src/main/java/es/cristoflop/poker/valormano/domain/checkers/CheckerEscalera.java
+++ src/main/java/es/cristoflop/poker/valormano/domain/checkers/CheckerEscalera.java
@@ -8,32 +8,65 @@ import java.util.List;
 
 public class CheckerEscalera extends Checker {
 
+    private final ValorJugada regular = ValorJugada.ESCALERA;
+    private final ValorJugada color = ValorJugada.ESCALERA_COLOR;
+    private final ValorJugada royal = ValorJugada.ESCALERA_REAL;
+
     public CheckerEscalera() {
-        this.valorJugada = ValorJugada.ESCALERA;
+        this.valorJugada = this.regular;
     }
 
     @Override
     protected List<Carta> cartasJugada(List<Carta> cartas) {
-        List<Carta> stair = new ArrayList<>();
-        boolean found = false;
-        int i = cartas.size() - 1;
-        while (i > 3 && !found) {
-            List<Carta> posibleStair = new ArrayList<>();
-            posibleStair.add(cartas.get(i));
-            int j = i - 1;
-            while (j >= 0 && posibleStair.size() < 5) {
-                if (posibleStair.get(posibleStair.size() - 1).esSiguiente(cartas.get(j))) {
-                    posibleStair.add(cartas.get(j));
-                }
-                j--;
-            }
-            if (posibleStair.size() == 5) {
-                found = true;
-                stair.addAll(posibleStair);
-            }
-            i--;
+        List<List<Carta>> posiblesEscaleras = new ArrayList<>();
+
+        for (int i = 0; i < cartas.size(); i++) { // posibles escaleras de tamaño 1
+            List<Carta> aux = new ArrayList<>();
+            aux.add(cartas.get(i));
+            posiblesEscaleras.add(aux);
         }
-        return stair;
+
+        int tamEscaleras = 1;
+        this.muestra(tamEscaleras, posiblesEscaleras);
+        while (tamEscaleras < 5 && !posiblesEscaleras.isEmpty()) {
+            posiblesEscaleras = this.addUnaCartaMasEnPosiblesEscalerasSiSePuede(posiblesEscaleras, cartas);
+            tamEscaleras++;
+            this.muestra(tamEscaleras, posiblesEscaleras);
+        }
+
+        List<Carta> bestStair = null;
+        if (!posiblesEscaleras.isEmpty()) bestStair = posiblesEscaleras.get(0);
+
+        return bestStair;
+    }
+
+    private List<List<Carta>> addUnaCartaMasEnPosiblesEscalerasSiSePuede(List<List<Carta>> posiblesEscaleras, List<Carta> cartas) {
+        List<List<Carta>> nuevasEscaleras = new ArrayList<>();
+        for (int i = 0; i < posiblesEscaleras.size(); i++) {
+            List<Carta> posibleEscaleraActual = posiblesEscaleras.get(i);
+            Carta ultimaCarta = posibleEscaleraActual.get(posibleEscaleraActual.size() - 1);
+            for (int j = 0; j < cartas.size(); j++) {
+                if (ultimaCarta.esSiguiente(cartas.get(j))) {
+                    List<Carta> nuevaPosibleEscalera = new ArrayList<>(posibleEscaleraActual);
+                    nuevaPosibleEscalera.add(cartas.get(j));
+                    nuevasEscaleras.add(nuevaPosibleEscalera);
+                }
+            }
+        }
+        return nuevasEscaleras;
+    }
+
+    private void muestra(int iteracion, List<List<Carta>> manos) {
+        System.out.println("Iteracion numero " + iteracion + " - POSIBLES ESCALERAS");
+        for (List<Carta> cartas : manos) {
+            for (Carta c : cartas) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("---------------------------");
+        System.out.println();
     }
 
 }
