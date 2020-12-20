5c99a3ab398ca13fcb9c76aa59a8ff10c7d72da7
cristoflop <cristofer.lc.99@gmail.com>
2020-12-20 16:14:48 +0100

    Refactor mano builder

--- src/test/java/es/cristoflop/poker/valormano/domain/ManoBuilder.java
+++ src/test/java/es/cristoflop/poker/valormano/domain/ManoBuilder.java
@@ -1,5 +1,10 @@
 package es.cristoflop.poker.valormano.domain;
 
+import es.cristoflop.poker.valormano.exception.ParserException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManoBuilder {
 
     private final StringBuilder cartas;
@@ -14,7 +19,24 @@ public class ManoBuilder {
     }
 
     Mano build() {
-        return new Mano(this.cartas.toString());
+        return new Mano(this.parse());
+    }
+
+    List<Carta> parse() {
+        List<Carta> cartas = new ArrayList<>();
+
+        int size = this.cartas.length();
+        if (size % 2 != 0) {
+            throw new ParserException("Formato invalido de mano");
+        }
+
+        for (int i = 0; i < size; i = i + 2) {
+            String cartaAux = this.cartas.substring(i, i + 2);
+            Carta carta = new Carta(cartaAux.charAt(0), cartaAux.charAt(1));
+            cartas.add(carta);
+        }
+
+        return cartas;
     }
 
 }
