270002d323673dd73f092ca35945b47851295c1d
cristoflop <cristofer.lc.99@gmail.com>
2021-02-03 15:13:20 +0100

    Added plays as entities

--- src/main/java/es/cristoflop/poker/valormano/domain/Jugada.java
+++ src/main/java/es/cristoflop/poker/valormano/domain/Jugada.java
@@ -1,28 +1,48 @@
 package es.cristoflop.poker.valormano.domain;
 
-import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Collections;
 import java.util.List;
 
+@Entity
 public class Jugada {
 
-    private final ValorJugada valorJugada;
-    private final List<Carta> cartasJugada;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private ValorJugada valorJugada;
+
+    private String cartasJugada;
+
+    public Jugada() {
+    }
 
     public Jugada(ValorJugada valorJugada, List<Carta> cartasJugada) {
         this.valorJugada = valorJugada;
-        this.cartasJugada = new ArrayList<>(cartasJugada);
+        StringBuilder builder = new StringBuilder();
+        cartasJugada.forEach(carta -> {
+            builder.append(carta.toString());
+        });
+        this.cartasJugada = builder.toString();
     }
 
     public static Jugada nullJugada() {
         return new Jugada(ValorJugada.NULL, Collections.emptyList());
     }
 
+    public long getId() {
+        return this.id;
+    }
+
     public ValorJugada getValorJugada() {
         return this.valorJugada;
     }
 
-    public List<Carta> getCartasJugada() {
+    public String getCartasJugada() {
         return this.cartasJugada;
     }
 
